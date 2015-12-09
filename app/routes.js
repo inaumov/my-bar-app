646450e403c3912fe8e12ddf1d59074006e7238b
Igor Naumov <naumov@live.com>
2015-12-09 23:16:05 +0200

    Fixed type error in express configuration (specified root to res.sendFile)

--- app/routes.js
+++ app/routes.js
@@ -23,7 +23,7 @@ module.exports = function (app) {
     // frontend routes =========================================================
     // route to handle all angular requests
     app.get('*', function (req, res) {
-        res.sendFile('./public/index.html'); // load our public/index.html file
+        res.sendFile('index.html', { root: '../public' }); // load our public/index.html file
     });
 
 };
